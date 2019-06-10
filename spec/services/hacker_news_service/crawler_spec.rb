023507376c82458f2a5687c4f38613a1f8d1bc20
NGUYEN CONG TRUONG <nctruong861990@gmail.com>
2019-06-10 15:49:48 +0700

    spec: Crawler#latest_news should return an array of HackerNew

--- spec/services/hacker_news_service/crawler_spec.rb
+++ spec/services/hacker_news_service/crawler_spec.rb
@@ -10,5 +10,13 @@ describe HackerNewsServices::Crawler do
     it 'returns data' do
       expect(@crawler.latest_news.blank?).to be(false)
     end
+
+    it 'returns an array' do
+      expect(@crawler.latest_news.is_a? Array).to be(true)
+    end
+
+    it 'returns an array of HackerNew' do
+      expect(@crawler.latest_news.first.is_a? HackerNew).to be(true)
+    end
   end
 end

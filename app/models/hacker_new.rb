698c8cc12d69a31e70bf799a74a48be5e1a1054d
NGUYEN CONG TRUONG <nctruong861990@gmail.com>
2019-06-11 17:20:30 +0700

    solve spec: hacker news's link is blank

--- app/models/hacker_new.rb
+++ app/models/hacker_new.rb
@@ -3,10 +3,11 @@ class HackerNew
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :title, :image, :excerpt
+  attr_accessor :title, :image, :excerpt, :link
 
   validates_presence_of :title
   validates_presence_of :excerpt
+  validates_presence_of :link
 
   def initialize(attributes = {})
     attributes.each do |name, value|

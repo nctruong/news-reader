d90ba23e89121d19022b26f3a4252d2eaae3882c
NGUYEN CONG TRUONG <nctruong861990@gmail.com>
2019-06-11 17:18:47 +0700

    spec: hacker news's link is blank

--- spec/models/hacker_new_spec.rb
+++ spec/models/hacker_new_spec.rb
@@ -3,22 +3,28 @@ require 'rails_helper'
 describe HackerNew, type: :model do
   describe 'valid' do
     it 'has title' do
-      news = HackerNew.new(title:'title', image:'image.png', excerpt: 'Excerpt')
+      news = HackerNew.new(title:'title', image:'image.png', excerpt: 'Excerpt', link: 'link')
       expect(news.valid?).to be(true)
     end
   end
 
   describe 'invalid' do
     it 'is invalid without a title' do
-      news = HackerNew.new(image:'image.png', excerpt: 'Excerpt')
+      news = HackerNew.new(image:'image.png', excerpt: 'Excerpt', link: 'link')
       news.valid?
       expect(news.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without a excerpt' do
-      news = HackerNew.new(image:'image.png', title: 'title')
+      news = HackerNew.new(image:'image.png', title: 'title', link: 'link')
       news.valid?
       expect(news.errors[:excerpt]).to include("can't be blank")
+    end
+
+    it 'is invalid without a link' do
+      news = HackerNew.new(image:'image.png', title: 'title', excerpt: 'excerpt')
+      news.valid?
+      expect(news.errors[:link]).to include("can't be blank")
     end
   end
 end
